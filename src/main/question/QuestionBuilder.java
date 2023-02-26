c4c9c5118797e8824263df7224236ad0b020465c
Pire <p.clement01@orange.fr>
2023-02-26 23:29:12 +0100

    Martin's tips

--- src/main/question/QuestionBuilder.java
+++ src/main/question/QuestionBuilder.java
@@ -12,11 +12,14 @@ public class QuestionBuilder {
     }
     public void addReponse(String direction, String nom){
         this.question.addReponse(direction, new Reponse(direction,nom));
+        return this;
     }
-    public void addEffet(String direction, TypeJauge jauge, Integer valeur){
+    public void addEffet(String direction, TypeJauge jauge, int valeur){
         this.question.getReponses().get(direction).putEffets(jauge,valeur);
+        return this;
     }
     public Question build(){
         return this.question;
     }
+
 }
