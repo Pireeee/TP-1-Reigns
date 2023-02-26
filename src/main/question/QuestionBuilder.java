915a4d1dc2b11daff5fa5b90d9062fea3d174240
Pire <p.clement01@orange.fr>
2023-02-26 23:44:15 +0100

    Martin's tip 2

--- src/main/question/QuestionBuilder.java
+++ src/main/question/QuestionBuilder.java
@@ -3,23 +3,32 @@ package main.question;
 import main.TypeJauge;
 
 public class QuestionBuilder {
-    public Question question;
 
-    public QuestionBuilder(String nomPersonnage,String question) {
+    private final Question question;
+
+    private QuestionBuilder(String nomPersonnage,String question) {
         this.question = new Question();
         this.question.nomPersonnage = nomPersonnage;
         this.question.question = question;
     }
-    public void addReponse(String direction, String nom){
+
+    public QuestionBuilder addReponse(String direction, String nom) {
         this.question.addReponse(direction, new Reponse(direction,nom));
         return this;
     }
-    public void addEffet(String direction, TypeJauge jauge, int valeur){
+
+    public QuestionBuilder addEffet(String direction, TypeJauge jauge, int valeur) {
         this.question.getReponses().get(direction).putEffets(jauge,valeur);
         return this;
     }
-    public Question build(){
+
+    public Question build() {
         return this.question;
     }
+
+    public static QuestionBuilder builder(String nomPersonnage, String question) {
+        return new QuestionBuilder(nomPersonnage, question);
+    }
+}
 
 }
