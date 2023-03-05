8c1387be5936ca8b2cd4fb9352a3d53445614b13
Pire <p.clement01@orange.fr>
2023-03-05 16:26:29 +0100

    Add questions with builder

--- src/main/question/BanqueQuestion.java
+++ src/main/question/BanqueQuestion.java
@@ -10,34 +10,33 @@ public class BanqueQuestion {
         //liste de questions
         List<Question> questions = new ArrayList<>();
         //commence le build de la question
-        QuestionBuilder question1 = new QuestionBuilder("Yves Jean","tu veux la guerre ?");
-        //on ajoute une réponse
-        question1.addReponse("G","Oui");
-        question1.addEffet("G", TypeJauge.ARMEE,+5);
-        question1.addEffet("G", TypeJauge.PEUPLE,-5);
-        question1.addReponse("D","Non");
-        question1.addEffet("G", TypeJauge.PEUPLE,+7);
-        questions.add(question1.build());
+        Question question1 = QuestionBuilder.builder("Yves Jean","tu veux la guerre ?")
+            .addReponse("G","Oui")
+            .addEffet("G", TypeJauge.ARMEE,+5)
+            .addEffet("G", TypeJauge.PEUPLE,-5)
+            .addReponse("D","Non")
+            .addEffet("G", TypeJauge.PEUPLE,+7)
+            .build();
+        questions.add(question1);
+        Question question2 =QuestionBuilder.builder("Paysan","Il n'y a plus rien à manger")
+                .addReponse("G","importer de la nourriture")
+                .addEffet("G", TypeJauge.FINANCE,-5)
+                .addEffet("G", TypeJauge.PEUPLE,+5)
+                .addReponse("D","Ne rien faire")
+                .addEffet("G", TypeJauge.PEUPLE,-7)
+                .build();
+        questions.add(question2);
+        Question question3 =QuestionBuilder.builder("Prêtre","Les dieux sont en colère")
+                .addReponse("G","Faire un sacrifice")
+                .addEffet("G", TypeJauge.CLERGE,+5)
+                .addEffet("G", TypeJauge.PEUPLE,-3)
+                .addReponse("D","Ne rien faire")
+                .addEffet("G", TypeJauge.CLERGE,-5)
+                .build();
+        questions.add(question2);
         return questions;
         /*
-        Question question2 = new Question(
-                "Paysan",
-                "Il n'y a plus rien à manger",
-                "Importer de la nourriture",
-                "Ne rien faire");
-        question2.ajouteEffetGauche(TypeJauge.FINANCE,-5);
-        question2.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question2.ajouteEffetDroite(TypeJauge.PEUPLE, -5);
-        questions.add(question2);
-        Question question3 = new Question(
-                "Prêtre",
-                "Les dieux sont en colère",
-                "Faire un sacrifice",
-                "Ne rien faire");
-        question3.ajouteEffetGauche(TypeJauge.CLERGE, +5);
-        question3.ajouteEffetGauche(TypeJauge.PEUPLE, -3);
-        question3.ajouteEffetDroite(TypeJauge.CLERGE, -5);
-        questions.add(question3);
+
         Question question4 = new Question(
                 "Main du roi",
                 "Le roi Baratheon rassemble son armée",
